2da8e5eefac8a082fa0b2e04a40d1b6a545e92f8
Ajibade Paul <ajibadeifedolapo17@gmail.com>
2025-08-24 22:47:21 +0100

    push

--- backend/server.js
+++ backend/server.js
@@ -50,16 +50,16 @@ app.use(cors(corsOptions));
 
 
 app.use('/api/users', userRoutes)
-if (process.env.NODE_ENV == 'production'){
-  const __dirname = path.resolve()
-  app.use(express.static(path.join(__dirname, 'frontend/dist')))
+// if (process.env.NODE_ENV == 'production'){
+//   const __dirname = path.resolve()
+//   app.use(express.static(path.join(__dirname, 'frontend/dist')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
-} else {
-  app.get("/", (req, res) => res.send('Server is ready'))
-}
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
+// } else {
+//   app.get("/", (req, res) => res.send('Server is ready'))
+// }
 
-
+app.get("/", (req, res) => res.send('Server is ready'))
 
 app.get("/health", (req, res) => res.status(200).send("OK"));
 
